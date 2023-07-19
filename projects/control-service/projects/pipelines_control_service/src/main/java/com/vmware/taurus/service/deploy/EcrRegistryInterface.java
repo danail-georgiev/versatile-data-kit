68758549b1626d6a633c365d18ef90ee179cbd41
Antoni Ivanov <aivanov@vmware.com>
2023-07-19 16:39:05 +0300

    control-service: job-builder uisng kaniko fix
    The way we are preparing our job-builder image is like this:
    
    ```
    FROM gcr.io/kaniko-project/executor
    
    FROM alpine
    
    COPY --from=0 /kaniko /kaniko
    ```
    
    This is apparently known issue :
    https://github.com/GoogleContainerTools/kaniko#known-issues
    
    > Running kaniko in any Docker image other than the official kaniko
    image is not supported (ie YMMV).
    > This includes copying the kaniko executables from the official image
    into another image.
    
    So instead making sure we built from kaniko image directly should fix
    the issue
    
    See https://github.com/vmware/versatile-data-kit/issues/2391

--- projects/control-service/projects/pipelines_control_service/src/main/java/com/vmware/taurus/service/deploy/EcrRegistryInterface.java
+++ projects/control-service/projects/pipelines_control_service/src/main/java/com/vmware/taurus/service/deploy/EcrRegistryInterface.java
@@ -5,16 +5,14 @@
 
 package com.vmware.taurus.service.deploy;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.services.ecr.AmazonECR;
 import com.amazonaws.services.ecr.AmazonECRClientBuilder;
-import com.amazonaws.services.ecr.model.DescribeImagesRequest;
-import com.amazonaws.services.ecr.model.DescribeImagesResult;
-import com.amazonaws.services.ecr.model.ImageIdentifier;
-import com.amazonaws.services.ecr.model.ImageNotFoundException;
-import com.amazonaws.services.ecr.model.RepositoryNotFoundException;
+import com.amazonaws.services.ecr.model.*;
+import com.vmware.taurus.exception.ExternalSystemError;
 import com.vmware.taurus.service.credentials.AWSCredentialsService;
 import com.vmware.taurus.service.credentials.AWSCredentialsService.AWSCredentialsDTO;
 import lombok.extern.slf4j.Slf4j;
@@ -29,19 +27,19 @@ import org.springframework.stereotype.Service;
 public class EcrRegistryInterface {
 
   public AWSStaticCredentialsProvider createStaticCredentialsProvider(
-      AWSCredentialsDTO awsCredentialsDTO) {
+          AWSCredentialsDTO awsCredentialsDTO) {
     if (!awsCredentialsDTO.awsSessionToken().isBlank()) {
       // need to include session token
       return new AWSStaticCredentialsProvider(
-          new BasicSessionCredentials(
-              awsCredentialsDTO.awsAccessKeyId(),
-              awsCredentialsDTO.awsSecretAccessKey(),
-              awsCredentialsDTO.awsSessionToken()));
+              new BasicSessionCredentials(
+                      awsCredentialsDTO.awsAccessKeyId(),
+                      awsCredentialsDTO.awsSecretAccessKey(),
+                      awsCredentialsDTO.awsSessionToken()));
     } else {
       // otherwise, we auth without session token
       return new AWSStaticCredentialsProvider(
-          new BasicAWSCredentials(
-              awsCredentialsDTO.awsAccessKeyId(), awsCredentialsDTO.awsSecretAccessKey()));
+              new BasicAWSCredentials(
+                      awsCredentialsDTO.awsAccessKeyId(), awsCredentialsDTO.awsSecretAccessKey()));
     }
   }
 
@@ -53,12 +51,12 @@ public class EcrRegistryInterface {
 
   private AmazonECR buildAmazonEcrClient(AWSCredentialsDTO awsCredentialsDTO) {
     AWSStaticCredentialsProvider awsStaticCredentialsProvider =
-        createStaticCredentialsProvider(awsCredentialsDTO);
+            createStaticCredentialsProvider(awsCredentialsDTO);
 
     return AmazonECRClientBuilder.standard()
-        .withCredentials(awsStaticCredentialsProvider)
-        .withRegion(awsCredentialsDTO.region())
-        .build();
+            .withCredentials(awsStaticCredentialsProvider)
+            .withRegion(awsCredentialsDTO.region())
+            .build();
   }
 
   private DescribeImagesRequest buildDescribeImagesRequest(String imageName) {
@@ -66,16 +64,36 @@ public class EcrRegistryInterface {
     // 850879199482.dkr.ecr.us-west-2.amazonaws.com/sc/dp/job-name:hash
     String imageRepoTag = extractImageRepositoryTag(imageName);
     ImageIdentifier imageIdentifier =
-        new ImageIdentifier().withImageTag(imageRepoTag.split(":")[1]);
+            new ImageIdentifier().withImageTag(imageRepoTag.split(":")[1]);
     String imageRepository = imageRepoTag.split(":")[0];
 
     return new DescribeImagesRequest()
-        .withRepositoryName(imageRepository)
-        .withImageIds(imageIdentifier);
+            .withRepositoryName(imageRepository)
+            .withImageIds(imageIdentifier);
   }
 
+  private static boolean existsRepository(AmazonECR ecrClient, String repositoryName) {
+    try {
+      ecrClient.describeRepositories(new DescribeRepositoriesRequest().withRepositoryNames(repositoryName));
+      return true;
+    } catch (RepositoryNotFoundException e) {
+      log.debug("Repository does not exist: {}", repositoryName);
+      return false;
+    } catch (Exception e) {
+      log.warn("Failed to check if image exists and will assume it doesn't exist. Exception: " + e);
+      return false;
+    }
+  }
+
+  /**
+   * Checks if a specific image exists in the Amazon ECR.
+   *
+   * @param imageName the name of the image whose existence is to be checked
+   * @param awsCredentialsDTO the DTO containing AWS credentials information
+   * @return true if the specified image exists, false otherwise
+   */
   public boolean checkEcrImageExists(
-      String imageName, AWSCredentialsService.AWSCredentialsDTO awsCredentialsDTO) {
+          String imageName, AWSCredentialsService.AWSCredentialsDTO awsCredentialsDTO) {
 
     AmazonECR ecrClient = buildAmazonEcrClient(awsCredentialsDTO);
     DescribeImagesRequest describeImagesRequest = buildDescribeImagesRequest(imageName);
@@ -86,10 +104,41 @@ public class EcrRegistryInterface {
         imageExists = true;
       }
     } catch (ImageNotFoundException | RepositoryNotFoundException e) {
-      log.info("Could not find image due to: {}", e);
+      log.info("Could not find image due to " + e);
     } catch (Exception e) {
-      log.error("Failed to check if image exists due to: ", e);
+      log.warn("Failed to check if image exists and will assume it doesn't exist. Exception: " + e);
     }
     return imageExists;
+  }
+
+  /**
+   * Creates a repository in Amazon ECR with the provided repository name.
+   * If a repository with the same name already exists, then nothing happens and operation succeeds.
+   *
+   * @param repositoryName the name of the repository to be created
+   * @param awsCredentialsDTO the DTO containing AWS credentials information
+   * @throws ExternalSystemError if other exception occurs during repository creation with container registry
+   */
+  public void createRepository(String repositoryName, AWSCredentialsService.AWSCredentialsDTO awsCredentialsDTO) {
+    AmazonECR ecrClient = buildAmazonEcrClient(awsCredentialsDTO);
+
+    try {
+
+      if (!existsRepository(ecrClient, repositoryName)) {
+        log.debug("Create ECR repository {}", repositoryName);
+        CreateRepositoryRequest createRepositoryRequest = new CreateRepositoryRequest()
+                .withRepositoryName(repositoryName);
+
+        CreateRepositoryResult createRepositoryResult = ecrClient.createRepository(createRepositoryRequest);
+
+        String repositoryUri = createRepositoryResult.getRepository().getRepositoryUri();
+        log.debug("ECR repository created: {}", repositoryUri);
+      }
+
+    } catch (AmazonClientException e) {
+      throw new ExternalSystemError(ExternalSystemError.MainExternalSystem.CONTAINER_REGISTRY,
+              "Creating container repository " + repositoryName + " failed.",
+              e);
+    }
   }
 }
